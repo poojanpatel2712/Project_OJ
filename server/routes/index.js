d9302dbb38cafdb9bc9cd976979bd17b380a041c
poojanpatel2712 <112116029@ece.iiitp.ac.in>
2023-06-26 20:14:27 +0530

    updated login and sign up page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import authRoutes from "./authRoutes.js";
-import problemRoutes from "./problemRoutes.js"
-import solutioRoutes from "./solutionRoutes.js"
-import editorialRoutes from "./editorialRoutes.js"
-import testcaseRoutes from "./testcaseRoutes.js"
+import problemRoutes from "./problemRoutes.js";
+import solutioRoutes from "./solutionRoutes.js";
+import editorialRoutes from "./editorialRoutes.js";
+import testcaseRoutes from "./testcaseRoutes.js";
 const router = express.Router();
 
-router.use("/auth",authRoutes);
-router.use("/problem",problemRoutes);
-router.use("/solutions",solutioRoutes);
-router.use("/editorials",editorialRoutes);
-router.use("/testcases",testcaseRoutes);
+router.use("/auth", authRoutes);
+router.use("/problem", problemRoutes);
+router.use("/solutions", solutioRoutes);
+router.use("/editorials", editorialRoutes);
+router.use("/testcases", testcaseRoutes);
 
 export default router;
