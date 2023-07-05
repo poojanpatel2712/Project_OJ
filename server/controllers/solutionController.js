8be38b992a9812a182a91293bdf7ba7a6e5afbdf
poojanpatel2712 <112116029@ece.iiitp.ac.in>
2023-07-05 13:20:44 +0530

    passport config

--- server/controllers/solutionController.js
+++ server/controllers/solutionController.js
@@ -25,4 +25,56 @@ const PostSolution = async (req, res) => {
   }
 };
 
-export { GetSolution, PostSolution };
+const submitProblem = async (req, res) => {
+  try {
+    const { language = "c++", code } = req.body;
+    const { problemId } = req.params;
+    const userId = req.session.passport.user._doc._id;
+
+    const testcases = await TestcasesModel.find({
+      problem: problemId,
+    });
+
+    if (!code) {
+      return res.status(404).json({ success: false, error: "Empty code !!" });
+    }
+
+    for (let i = 0; i < testcases.length; i++) {
+      const testcase = testcases[i];
+      const filePath = await generateFile(language, code);
+      const output = await executeCpp(filePath, testcase.input);
+      const pureStringOutout = output.replace(/(?:\r\n|\r|\n)/g, "");
+      const pureStringtestcaseOutput = testcase.output.replace(
+        /(?:\r\n|\r|\n)/g,
+        ""
+      );
+      const success =
+        pureStringOutout.toLowerCase() ==
+        pureStringtestcaseOutput.toLowerCase();
+
+      if (!success) {
+        const solution = await SolutionsModel.create({
+          problem: problemId,
+          verdict: "Fail",
+          message: `Wrong Answer on testcase ${index + 1}`,
+          submittedBy: userId,
+          submittedAt: new Date(),
+        });
+        return res.staus(200).json({ solution });
+      }
+    }
+    const solution = await SolutionsModel.create({
+      problem: problemId,
+      verdict: "Pass",
+      message: `Accepted`,
+      submittedBy: userId,
+      submittedAt: new Date(),
+    });
+
+    return res.status(200).json({ solution });
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+};
+
+export { GetSolution, PostSolution, submitProblem };
